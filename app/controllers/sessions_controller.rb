52ed35eef484587fd728b6f0263a7fe44a254b21
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-21 06:09:03 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end 
   
   def create 
-     if !params[:name] || params[:name].empty?
+     if  params[:name].empty?
      redirect_to ({controller: 'sessions', action: 'new'})
     else
        session[:name] = params[:name]
